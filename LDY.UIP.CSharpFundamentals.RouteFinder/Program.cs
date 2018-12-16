f327c6999fc1a6c4c7bb10fec52290cd36ba0cfe
LDY <liashkod@gmail.com>
2018-12-16 16:54:22 +0200

    Refactored Code

--- LDY.UIP.CSharpFundamentals.RouteFinder/Program.cs
+++ LDY.UIP.CSharpFundamentals.RouteFinder/Program.cs
@@ -25,14 +25,15 @@ namespace LDY.UIP.CSharpFundamentals.RouteFinder {
 
             AssignStartPoint(map, startPoint);
 
-            return CreateRouteRecursively(true, map, startPoint, startPoint, finishPoint, 1);
-        }
+            int[][] createdRoute = CreateRouteRecursively(true, map, startPoint, startPoint, finishPoint, 1);
 
-        private static void AssignStartPoint(int[][] map, int[] startPoint) {
-            int startPointX = startPoint[0];
-            int startPointY = startPoint[1];
+            string resultMessage = 
+                createdRoute == null ?
+                "Please modify the algorithm because a deadlock happened" :
+                "Finally, we reached the goal!!!!";
+            Console.WriteLine(resultMessage);
 
-            map[startPointX][startPointY] = 1;
+            return createdRoute;
         }
 
         private static int[][] CreateRouteRecursively(bool isRootCall, int[][] map, int[] currentPoint, int[] startPoint, int[] finishPoint, int iteration) {
@@ -42,33 +43,20 @@ namespace LDY.UIP.CSharpFundamentals.RouteFinder {
             string[] directions = new string[] { "Right", "Left", "Up", "Down" };
 
             foreach (string directionName in directions) {
-                int[] suggestedPoint = GetSuggestedPoint(map, currentPoint, directionName);
-                bool isDirectionAllowed = IsPointAllowedToMove(map, suggestedPoint, startPoint);
+                int[] suggestedPointToMove = GetSuggestedPointToMove(map, currentPoint, directionName);
+                bool isDirectionAllowed = IsPointAllowedToMove(map, suggestedPointToMove, startPoint);
                 if (isDirectionAllowed && !isFinishReached) {
-                    MarkPointAsReached(map, suggestedPoint);
-                    CreateRouteRecursively(false, map, suggestedPoint, startPoint, finishPoint, ++iteration);
+                    MarkPointAsReached(map, suggestedPointToMove);
+                    CreateRouteRecursively(false, map, suggestedPointToMove, startPoint, finishPoint, ++iteration);
                 }
-
                 isFinishReached = IsFinishReached(map, finishPoint);
-
-                if (isFinishReached) {
-                    break;
-                }
+                if (isFinishReached) { return map; }
             }
-
-            if (isFinishReached) {
-                if (isRootCall) {
-                    Console.WriteLine("Finally, we reached the goal!!!!");
-                }
-                return map;
-
-            } else {
-                if (isRootCall) {
-                    throw new Exception("Please modify the algorithm because a deadlock happened");
-                }
+                       
+            if (!isRootCall) {
                 Console.WriteLine($"!!!! [{currentPoint[0]},{currentPoint[1]}] It's a wrong way, let's go back and try to find new one");
-                return null;
             }
+            return null;
         }
 
         private static bool IsFinishReached(int[][] map, int[] finishPoint) {
@@ -83,28 +71,31 @@ namespace LDY.UIP.CSharpFundamentals.RouteFinder {
             map[pointY][pointX] = 1;
         }
 
-        private static int[] GetSuggestedPoint(int[][] map, int[] currentPoint, string direction) {
-            int[] suggestedPoint = new int[] { currentPoint[0], currentPoint[1] };
+        private static void AssignStartPoint(int[][] map, int[] startPoint) {
+            MarkPointAsReached(map, startPoint);
+        }
+
+        private static int[] GetSuggestedPointToMove(int[][] map, int[] currentPoint, string direction) {
+            int[] suggestedPointToMove = new int[] { currentPoint[0], currentPoint[1] };
             switch (direction) {
                 case "Right":
-                    suggestedPoint[1]++;
+                    suggestedPointToMove[1]++;
                     break;
                 case "Left":
-                    suggestedPoint[1]--;
+                    suggestedPointToMove[1]--;
                     break;
                 case "Up":
-                    suggestedPoint[0]++;
+                    suggestedPointToMove[0]++;
                     break;
                 case "Down":
-                    suggestedPoint[0]--;
+                    suggestedPointToMove[0]--;
                     break;
                 default:
-                    suggestedPoint[0] = int.MaxValue;
-                    suggestedPoint[1] = int.MaxValue;
+                    suggestedPointToMove[0] = int.MaxValue;
+                    suggestedPointToMove[1] = int.MaxValue;
                     break;
             }
-
-            return suggestedPoint;
+            return suggestedPointToMove;
         }
 
         private static bool IsPointAllowedToMove(int[][] map, int[] suggestedPoint, int[] startPoint) {
@@ -112,11 +103,7 @@ namespace LDY.UIP.CSharpFundamentals.RouteFinder {
             int pointX = suggestedPoint[1];
             bool isSuggestedEqualStartPoint = suggestedPoint[0] == startPoint[0] && suggestedPoint[1] == startPoint[1];
             bool isPointOutOfRange = pointY < 0 || pointY >= map.Length || pointX < 0 || pointX >= map[pointY].Length;
-            if (isSuggestedEqualStartPoint || isPointOutOfRange) {
-                return false;
-            }
-            int pointvalue = map[pointY][pointX];
-            return pointvalue != -1 && pointvalue != 1;
+            return !isSuggestedEqualStartPoint && !isPointOutOfRange && map[pointY][pointX] == 0;
         }
 
         private static void PrintMap(int[][] map, int iteration) {
