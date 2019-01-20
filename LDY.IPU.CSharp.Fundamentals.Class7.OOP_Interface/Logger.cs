ac09f18a5f671ddfd4b883c8df15928fd4b95555
LDY <liashkod@gmail.com>
2019-01-20 15:20:45 +0200

    Cleared Code

--- LDY.IPU.CSharp.Fundamentals.Class7.OOP_Interface/Logger.cs
+++
@@ -1,9 +0,0 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Text;
-using System.Threading.Tasks;
-
-namespace LDY.IPU.CSharp.Fundamentals.Class7.OOP_Interface {
-    
-}
