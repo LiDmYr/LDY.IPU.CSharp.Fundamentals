c285671b7efa4e21a9e69b68cc46dea572370096
LDY <liashkod@gmail.com>
2019-04-20 11:21:18 +0300

    Shared EarthCalc initial solution

---
+++ LDY.UIP.EarthCalculator.BLL/Services/Logger.cs
@@ -0,0 +1,65 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LDY.UIP.EarthCalculator.BLL.Interfaces;
+
+namespace LDY.UIP.EarthCalculator.BLL.Services {
+    public class Logger : ILogger {
+        // public List<ILogStorage> LogStorages = new List<ILogStorage>();
+
+        private int _logLevel = (int)Level.info;
+
+        public int LogLevel {
+            get {
+                return (int)_logLevel;
+            }
+            set {
+                if (value > 5 && value < 1) {
+                    return;
+                }
+                this._logLevel = value;
+            }
+        }
+
+
+        private void PrintMessage(string messageType, string message, int currentLogLevel) {
+            if (this.LogLevel > currentLogLevel) {
+                return;
+            }
+
+            //foreach (ILogStorage logStorage in LogStorages) {
+            //    logStorage.PrintMessage($"{DateTime.Now}: {messageType}: {message}");
+            //}
+        }
+
+        public void Debug(string message) {
+            PrintMessage("DEBUG", message, (int)Level.debug);
+        }
+
+        public void Error(string message) {
+            PrintMessage("Error", message, (int)Level.error);
+        }
+
+        public void Fatal(string message) {
+            PrintMessage("Fatal", message, (int)Level.fatal);
+        }
+
+        public void Info(string message) {
+            PrintMessage("Info", message, (int)Level.info);
+        }
+
+        public void Warn(string message) {
+            PrintMessage("Warn", message, (int)Level.warning);
+        }
+    }
+
+    public enum Level {
+        info = 1,
+        debug = 2,
+        warning = 3,
+        error = 4,
+        fatal = 5
+    }
+}
---
+++ LDY.UIP.EarthCalculator.UIConsole/Program.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using LDY.UIP.EarthCalculator.BLL.Services;
+
+namespace LDY.UIP.EarthCalculator.UIConsole {
+    class Program {
+        static void Main(string[] args) {
+            Logger log = new Logger();
+            log.Info("text");
+            // log.
+        }
+    }
+}
